ad829cc7c56ef5a23de2c778493f7a11e08f2360
Berelindis <romain.bellande@orange.fr>
2017-05-20 22:46:56 +0200

    update travis deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 git add .
-git commit -m "Deployment"
+git commit -m "deployment"
 git status
 eval "$(ssh-agent -s)" #start the ssh agent
-git remote add deploy ssh://deploy@46.101.53.213:/var/www/ngx-loopback
-git push deploy HEAD:master --force
+slc build --commit --onto deploy --install
+slc deploy --service=ngx-loopback http://46.101.55.92:32768
 echo "deployment completed"
