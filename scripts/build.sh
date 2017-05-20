adc726fffd56bea0194248726f821c13a4d196f7
Berelindis <romain.bellande@orange.fr>
2017-05-20 23:37:08 +0200

    add some git rm

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 yarn global add @angular/cli
 npm run dist
-# rm deploy_key.enc
-# rm google-chrome-stable_current_amd64.deb
 rm .gitignore
 mv .travis-gitignore .gitignore
+git rm google-chrome-stable_current_amd64.deb
+git rm deploy_key.enc
+git rm -rf angular
 echo "build completed"
