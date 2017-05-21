984ab420e456f30b328f2fe073e3be53ee41c51a
Berelindis <romain.bellande@orange.fr>
2017-05-21 03:03:10 +0200

    update travis deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 eval "$(ssh-agent -s)" #start the ssh agent
-git clean -x -d
+git clean -xdf
 slc build --commit --onto deploy --install
 slc deploy --service=ngx-loopback http://46.101.55.92:8701
 echo "deployment completed"
