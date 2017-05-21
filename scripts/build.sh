dc09b93fda5abc7d3de6634d3f2a8dfa81575799
Berelindis <romain.bellande@orange.fr>
2017-05-21 03:14:56 +0200

    update deployment

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 yarn global add @angular/cli
 npm run dist
-rm .gitignore
-mv .travis-gitignore .gitignore
-# git rm google-chrome-stable_current_amd64.deb
-# git rm deploy_key.enc
-# git rm -rf angular
 echo "build completed"
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 eval "$(ssh-agent -s)" #start the ssh agent
+rm .gitignore
+mv .travis-gitignore .gitignore
+git add .
 git clean -xdf
 slc build --commit --onto deploy --install
 slc deploy --service=ngx-loopback http://46.101.55.92:8701
